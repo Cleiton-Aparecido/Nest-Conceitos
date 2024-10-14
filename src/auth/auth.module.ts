08175ea98c43e4741cba5651121ba8414a895958
cleitonfonseca <cleitonfonseca@boltcard.com.br>
2024-10-14 14:51:01 -0300

    Alterado do prisma para o type orm

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,23 @@
-import { Module } from "@nestjs/common";
-import { JwtModule } from "@nestjs/jwt";
-import { AuthController } from "./auth.controller";
-import { UserModule } from "src/user/user.module";
-import { PrismaModule } from "src/prisma/prisma.module";
-import { AuthService } from "./auth.service";
+import { Module, forwardRef } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '../user/user.module';
+import { FileModule } from '../file/file.module';
+import { UserEntity } from '../user/entity/user.entity';
 
 @Module({
-    imports:[JwtModule.register({
-        secret: `cALlaPR5Nh3ahEC`
+  imports: [
+    JwtModule.register({
+      secret: String(process.env.JWT_SECRET),
     }),
-    UserModule,
-    PrismaModule
-
-],
-    controllers:[AuthController],
-    providers:[AuthService]
+    forwardRef(() => UserModule),
+    FileModule,
+    TypeOrmModule.forFeature([UserEntity]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
 })
-
-export class AuthModule{
-
-}
+export class AuthModule {}
